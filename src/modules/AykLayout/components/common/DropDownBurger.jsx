ff58258a3eba11cd9f291f9844140bf1d5cc8b34
vip <mostafa.ali.mohamed@gmail.com>
2018-06-30 23:00:07 +0200

    All branches merged

--- src/modules/AykLayout/components/common/DropDownBurger.jsx
+++ src/modules/AykLayout/components/common/DropDownBurger.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Dropdown, Icon } from 'semantic-ui-react';
 
+import PropTypes from 'prop-types';
+
 const trigger = (
   <span>
     <Icon name="bars" />
   </span>
 );
-const DropDownBurger = () => (
-  <Dropdown trigger={trigger} />
+const DropDownBurger = ({ handleClick }) => (
+  <Dropdown trigger={trigger} onClick={handleClick} />
 );
+
+DropDownBurger.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default DropDownBurger;
