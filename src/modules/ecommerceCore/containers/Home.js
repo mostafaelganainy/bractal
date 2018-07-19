d5d304a27cfa3c8d37b1d61f3710094e0796287b
aya.adel <aya.adel@badrit.com>
2018-07-19 10:53:03 +0200

    Merge branch 'AccountTest' into origin/development

--- src/modules/ecommerceCore/containers/Home.js
+++ src/modules/ecommerceCore/containers/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { translate } from 'react-i18next';
-import ProductsListPage from '~/modules/ecommerceCoreUI/containers/ProductsListPage';
+// import ProductsListPage from '~/modules/ecommerceCoreUI/containers/ProductsListPage';
 import SideMenu from '~/modules/ecommerceCoreUI/components/header/sideMenu/SideMenu';
 import Brands from '~/modules/ecommerceCoreUI/components/brands/Brands';
 import HomeMainSlider from '~/modules/ecommerceCoreUI/components/HomeMainSlider/HomeMainSlider';
@@ -12,8 +12,10 @@ const HomePage = () =>
       <div style={{ width: '300px' }}>
         <SideMenu />
       </div>
-      <ProductsListPage headerTitle="Most Popular" headerSubtitle="Enjoy our popular products" />
-      <ProductsListPage headerTitle="New Arrivals" headerSubtitle="Check our New Arrival Products" />
+      {/* <ProductsListPage headerTitle="Most Popular"
+      headerSubtitle="Enjoy our popular products" />
+      <ProductsListPage headerTitle="New Arrivals"
+      headerSubtitle="Check our New Arrival Products" /> */}
       <Brands />
     </React.Fragment>
   );
